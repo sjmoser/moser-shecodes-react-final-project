a8c11994436b5d8af482517e8313ff6cf721a668
sjmoser <sarahjeanemoser@gmail.com>
2021-11-27 15:24:20 -0700

    Added the GitHub URL

--- src/App.js
+++ src/App.js
@@ -2,5 +2,19 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Sarah Moser and is{" "}
+        <a
+          href="https://github.com/sjmoser/moser-shecodes-react-final-project"
+          target="_blank"
+        >
+          open-sourced on GitHub
+        </a>
+        .
+      </footer>
+    </div>
+  );
 }
