b7d1816725c2a3fd0d2ee9ce3999762e88e00163
sjmoser <sarahjeanemoser@gmail.com>
2021-11-28 10:23:57 -0700

    Added bootstrap to my project

--- src/App.js
+++ src/App.js
@@ -4,14 +4,16 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Sarah Moser and is{" "}
-        <a href="https://github.com/sjmoser/moser-shecodes-react-final-project">
-          open-sourced on GitHub
-        </a>
-        .
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by Sarah Moser and is{" "}
+          <a href="https://github.com/sjmoser/moser-shecodes-react-final-project">
+            open-sourced on GitHub
+          </a>
+          .
+        </footer>
+      </div>
     </div>
   );
 }
