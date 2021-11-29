2e2664f5e52a76978870e0c08a66ba1ac8d65fab
sjmoser <sarahjeanemoser@gmail.com>
2021-11-28 20:42:27 -0700

    Fixed links in footer

--- src/App.js
+++ src/App.js
@@ -8,13 +8,25 @@ export default function App() {
       <div className="container">
         <Weather defaultCity="Paris" />
         <footer>
-          This project was coded by Sarah Moser and is{" "}
+          This project was coded by{" "}
+          <a
+            href="https://github.com/sjmoser"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Sarah Moser
+          </a>{" "}
+          and is{" "}
           <a
             href="https://github.com/sjmoser/moser-shecodes-react-final-project"
             target="_blank"
             rel="noopener noreferrer"
           >
             open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a href="/" target="_blank" rel="noopener noreferrer">
+            hosted on Netlify
           </a>
           .
         </footer>
