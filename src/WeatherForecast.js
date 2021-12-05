21f9008ca835430cbdbf51253b34d01f3aac9b2e
sjmoser <sarahjeanemoser@gmail.com>
2021-12-05 12:56:53 -0700

    Added forecast and made adjustments to icon and API integration

---
+++ src/WeatherForecast.js
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import WeatherForecastDay from "./WeatherForecastDay";
+import "./WeatherForecast.css";
+import axios from "axios";
+
+export default function WeatherForecast(props) {
+  let [loaded, setLoaded] = useState(false);
+  let [forecast, setForecast] = useState(null);
+
+  function handleResponse(response) {
+    setForecast(response.data.daily);
+    setLoaded(true);
+  }
+  if (loaded) {
+    return (
+      <div className="WeatherForecast">
+        <div className="row">
+          <div className="col">
+            <WeatherForecastDay data={forecast[0]} />
+          </div>
+        </div>
+      </div>
+    );
+  } else {
+    let apiKey = "3c20049e4b9dc7307aa7da0d29accbc2";
+    let longitude = props.coordinates.lon;
+    let latitude = props.coordinates.lat;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric
+`;
+    axios.get(apiUrl).then(handleResponse);
+    return null;
+  }
+}
